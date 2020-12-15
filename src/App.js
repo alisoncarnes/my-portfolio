380e8ccbb0ea41d80b380bcaf907827e1b335bd6
Alison Carnes <alisoncarnes@alisons-air.lan>
2020-12-15 12:33:19 -0800

    started projects page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
@@ -32,6 +33,7 @@ class App extends Component {
                   <Route path='/projects' component={Projects} />
                   <Route path='/resume' component={Resume} />
               </Switch>
+              <Footer/>
           </div>
     </Router>
     );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,9 +3,91 @@ import React, { Component } from 'react';
 class Projects extends Component {
   render() {
     return (
-        <div>
-          <h2>Projects</h2>
+      <div className="main">
+
+        <h2>Projects</h2>
+        <text className="text-content">All projects listed below were created entirely by me unless stated otherwise.</text>
+
+        <div className="container">
+
+          <div className="projects-page">
+
+            <div className="project-page__image">
+
+              <img src="https://i.ibb.co/3mFMN5h/perrenial.jpg"/>
+
+            </div>
+
+            <div class="projects-page__text">
+              <p>
+                <h3>perrenial</h3>
+                <a href="https://perrenial.herokuapp.com/plants">https://perrenial.herokuapp.com</a>
+                <br/><br/>
+                Technologies used:
+                <br/>
+                <text className="text-content">Node.js, Mongoose, Express & EJS</text>
+                <br/><br/>
+                About the Project:
+                <br/>
+                <text className="text-content">Site Description here</text>
+              </p>
+              </div>
+            </div>
+
+            <div className="projects-page">
+
+                <div className="project-page__image">
+
+                  <img src="https://i.ibb.co/NNVXq0b/moss-Jobs2.png"/>
+
+                </div>
+
+                <div class="projects-page__text">
+                <p>
+                    <h3>mossJobs</h3>
+                    <a href="https://limitless-oasis-80314.herokuapp.com/">https://limitless-oasis-80314.herokuapp.com</a>
+                    <br/><br/>
+                    Technologies used:
+                    <br/>
+                    <text className="text-content">PHP, React.js, Materialize CSS</text>
+                    <br/><br/>
+                    About the Project:
+                    <br/>
+                    <text className="text-content">Site Description here</text>
+                    <br/><br/>
+                    <text className="text-content">Created with: <a href="https://github.com/onerivas">Juan Carlos Rivas</a></text>
+                  </p>
+                </div>
+
+          </div>
+
+          <div className="projects-page">
+
+              <div className="project-page__image">
+
+                <img src="https://i.ibb.co/NLtrvc6/dailybeer.png"/>
+
+              </div>
+
+              <div class="projects-page__text">
+              <p>
+                  <h3>daily beer</h3>
+                  <a href="https://dailybeer.herokuapp.com/">https://dailybeer.herokuapp.com</a>
+                  <br/><br/>
+                  Technologies used:
+                  <br/>
+                  <text className="text-content">MongoDB, React.js, Node.js, Express & Bootstrap</text>
+                  <br/><br/>
+                  About the Project:
+                  <br/>
+                  <text className="text-content">Site Description here</text>
+                </p>
+              </div>
+
         </div>
+
+        </div>
+      </div>
     );
   }
 }
