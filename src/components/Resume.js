59a2899f639126052901ac03c446301da1ce51fa
Alison Carnes <alisoncarnes@alisons-air.lan>
2021-01-06 12:40:42 -0800

    edited resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -18,12 +18,13 @@ class Resume extends Component {
             <div className="empty-div"/>
             <div className="row">
               <div className="col">
-              I am a software engineer who is interested in leveraging full stack web development and industry leading design tools and technologies to build user-centric products.
+              <p>I am a software engineer who is interested in leveraging full stack web development and industry leading design tools and technologies to build user-centric products.</p>
               </div>
+              <div className="empty-div"/>
               <div className="col">
-              Email: <a href="mailto:alisoncarnes@gmail.com">alisoncarnes@gmail.com</a><br/>
+              <p>Email: <a href="mailto:alisoncarnes@gmail.com">alisoncarnes@gmail.com</a><br/>
               GitHub: <a href="https://github.com/alisoncarnes">@alisoncarnes</a> <br/>
-              LinkedIn: <a href="https://www.linkedin.com/in/alison-carnes-a7100b119/">@alison-carnes-a7100b199</a> <br/>
+              LinkedIn: <a href="https://www.linkedin.com/in/alison-carnes-a7100b119/">@alison-carnes-a7100b199</a> </p>
               </div>
             </div>
             <div className="empty-div"/>
@@ -31,19 +32,19 @@ class Resume extends Component {
             <div className="empty-div"/>
             <div className="row">
               <div className="col">
-              <h4>Languages:</h4> <br/>
-              <b>JavaScript, HTML, CSS, PHP, SQL</b>
+              <h4>Languages:</h4>
+              <p>JavaScript, HTML, CSS, PHP, SQL</p>
 
               </div>
               <div className="col">
-              <h4>Frameworks:</h4> <br/>
-              <b>React.js, MongoDB, API/Rest APIs, Mongoose ODM, Node.js, GitHub, jQuery</b>
+              <h4>Frameworks:</h4>
+              <p>React.js, MongoDB, API/Rest APIs, Mongoose ODM, Node.js, GitHub, jQuery</p>
               </div>
             </div>
             <div className="empty-div"/>
             <h4>Work Experience:</h4>
             <div className="empty-div"/>
-            <h5>Practice of Dr. Steven Shimoyama</h5>
+            <h5>Practice of Dr. Steven Shimoyama</h5><p>
             Marina del Rey, CA<br/>
             Lead X-Ray Technician<br/>
             August '17 - Feb '20<br/>
@@ -52,6 +53,7 @@ class Resume extends Component {
               <li>Consultations with lead specialist regarding x-ray interpretations and outcomes</li>
               <li>Explanation of x-ray results and potential next steps for patients</li>
             </ul>
+            </p>
 
 
 
@@ -60,10 +62,12 @@ class Resume extends Component {
             <div className="empty-div"/>
 
             <h5>General Assembly</h5>
+            <p>
             Award winning, industry leading software immersive program with a focus on HTML, CSS, JavaScript, PHP, modern front-end frameworks, back-end web applications and API development with modern programming languages and MVC, and consummation of third-party APIs. GA also focused on collaboration in teams using agile development workflow and widely accepted collaboration practices in Git/Github
             <br/><br/>
             <b>Software Engineering</b><br/>
             August '20 - Nov '20
+            </p>
         </div>
     );
   }
