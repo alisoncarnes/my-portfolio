8cfc940f9d8461195d44d310f0c741b00f247485
Alison Carnes <alisoncarnes@alisons-air.lan>
2021-01-06 12:29:11 -0800

    updated projects and resume page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -23,13 +23,13 @@ class Projects extends Component {
                 <h3>perrenial</h3>
                 <a href="https://perrenial.herokuapp.com/plants">https://perrenial.herokuapp.com</a>
                 <br/><br/>
-                Technologies used:
+                <b>Technologies used:</b>
                 <br/>
-                <text className="text-content">Node.js, Mongoose, Express & EJS</text>
+                <text className="text-content">HTML, CSS, Node.js, Mongoose, Express & EJS</text>
                 <br/><br/>
-                About the Project:
+                <b>About the Project:</b>
                 <br/>
-                <text className="text-content">Site Description here</text>
+                <text className="text-content">Perrenial is a full-stack application of a mock plant shop displaying all 7 RESTful routes and full CRUD. The back-end was built using Express, Method-Override and Mongoose. Front-end was created with EJS, Bootstrap and CSS. This project also displays user authentication using Express-Session.  </text>
               </p>
               </div>
             </div>
@@ -47,13 +47,13 @@ class Projects extends Component {
                     <h3>mossJobs</h3>
                     <a href="https://limitless-oasis-80314.herokuapp.com/">https://limitless-oasis-80314.herokuapp.com</a>
                     <br/><br/>
-                    Technologies used:
+                    <b>Technologies used:</b>
                     <br/>
-                    <text className="text-content">PHP, React.js, Materialize CSS</text>
+                    <text className="text-content">HTML, CSS, JavaScript, Heroku, PHP, PostgreSQL, React.js & Materialize</text>
                     <br/><br/>
-                    About the Project:
+                    <b>About the Project:</b>
                     <br/>
-                    <text className="text-content">Site Description here</text>
+                    <text className="text-content">For this project created with a fellow student, we wanted to create a space for our fellow classmates to share job opportunities that they may not find compatible for them, but may be for someone else. In this project, I created the PostgreSQL database and inputed the table, as well as the front-end and styling of the application.</text>
                     <br/><br/>
                     <text className="text-content">Created with: <a href="https://github.com/onerivas">Juan Carlos Rivas</a></text>
                   </p>
@@ -74,13 +74,13 @@ class Projects extends Component {
                   <h3>daily beer</h3>
                   <a href="https://dailybeer.herokuapp.com/">https://dailybeer.herokuapp.com</a>
                   <br/><br/>
-                  Technologies used:
+                  <b>Technologies used:</b>
                   <br/>
-                  <text className="text-content">MongoDB, React.js, Node.js, Express & Bootstrap</text>
+                  <text className="text-content">MongoDB, React.js, Node.js, Express, Bootstrap & CSS</text>
                   <br/><br/>
-                  About the Project:
+                  <b>About the Project:</b>
                   <br/>
-                  <text className="text-content">Site Description here</text>
+                  <text className="text-content">Daily Beer is an application where a user can search for beers based off of ABV or find breweries from searching by city. The app follows the 7 RESTful routes and demonstrates full CRUD. As for the search options, I used two third-party APIs. The application is also responsive. </text>
                 </p>
               </div>
 
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,8 +3,67 @@ import React, { Component } from 'react';
 class Resume extends Component {
   render() {
     return (
-        <div>
+        <div className="container">
+
           <h2>Resume</h2>
+            <div className="empty-div"/>
+            <div className="row">
+              <div className="col">
+              <h3>Alison Carnes</h3>
+              </div>
+              <div className="col">
+              attachment to resume here
+              </div>
+            </div>
+            <div className="empty-div"/>
+            <div className="row">
+              <div className="col">
+              I am a software engineer who is interested in leveraging full stack web development and industry leading design tools and technologies to build user-centric products.
+              </div>
+              <div className="col">
+              Email: <a href="mailto:alisoncarnes@gmail.com">alisoncarnes@gmail.com</a><br/>
+              GitHub: <a href="https://github.com/alisoncarnes">@alisoncarnes</a> <br/>
+              LinkedIn: <a href="https://www.linkedin.com/in/alison-carnes-a7100b119/">@alison-carnes-a7100b199</a> <br/>
+              </div>
+            </div>
+            <div className="empty-div"/>
+            <h3>Technical Skills</h3>
+            <div className="empty-div"/>
+            <div className="row">
+              <div className="col">
+              <h4>Languages:</h4> <br/>
+              <b>JavaScript, HTML, CSS, PHP, SQL</b>
+
+              </div>
+              <div className="col">
+              <h4>Frameworks:</h4> <br/>
+              <b>React.js, MongoDB, API/Rest APIs, Mongoose ODM, Node.js, GitHub, jQuery</b>
+              </div>
+            </div>
+            <div className="empty-div"/>
+            <h4>Work Experience:</h4>
+            <div className="empty-div"/>
+            <h5>Practice of Dr. Steven Shimoyama</h5>
+            Marina del Rey, CA<br/>
+            Lead X-Ray Technician<br/>
+            August '17 - Feb '20<br/>
+            <ul>
+              <li>Preparation and procurement of upper/lower extremity, chest & spine x-rays for geriatric and professional athletic patients</li>
+              <li>Consultations with lead specialist regarding x-ray interpretations and outcomes</li>
+              <li>Explanation of x-ray results and potential next steps for patients</li>
+            </ul>
+
+
+
+            <div className="empty-div"/>
+            <h4>Education:</h4>
+            <div className="empty-div"/>
+
+            <h5>General Assembly</h5>
+            Award winning, industry leading software immersive program with a focus on HTML, CSS, JavaScript, PHP, modern front-end frameworks, back-end web applications and API development with modern programming languages and MVC, and consummation of third-party APIs. GA also focused on collaboration in teams using agile development workflow and widely accepted collaboration practices in Git/Github
+            <br/><br/>
+            <b>Software Engineering</b><br/>
+            August '20 - Nov '20
         </div>
     );
   }
